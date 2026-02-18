f178ba3146942f35a54208623abd2014096ac842
Kalana Ratnayake <kalanaratnayake95@gmail.com>
2026-02-18 14:43:12 +0000

    seperated launch file

--- anygrasp_ros/launch/detection.launch.py
+++ anygrasp_ros/launch/detection.launch.py
@@ -9,7 +9,6 @@ def generate_launch_description() -> LaunchDescription:
     depth_topic = LaunchConfiguration('depth_topic')
 
     detection_checkpoint = LaunchConfiguration('detection_checkpoint')
-    tracking_checkpoint = LaunchConfiguration('tracking_checkpoint')
 
     fx = LaunchConfiguration('fx')
     fy = LaunchConfiguration('fy')
@@ -39,40 +38,16 @@ def generate_launch_description() -> LaunchDescription:
         ],
     )
 
-    tracking_node = Node(
-        package='anygrasp_ros',
-        executable='anygrasp_tracking_node',
-        name='anygrasp_tracking_node',
-        output='screen',
-        parameters=[
-            {
-                'checkpoint_path': tracking_checkpoint,
-                'fx': fx,
-                'fy': fy,
-                'cx': cx,
-                'cy': cy,
-                'depth_scale': depth_scale,
-            }
-        ],
-        remappings=[
-            ('rgb_image', rgb_topic),
-            ('depth_image', depth_topic),
-            ('tracking', '/anygrasp/tracking'),
-        ],
-    )
-
     return LaunchDescription(
         [
             DeclareLaunchArgument('rgb_topic', default_value='/rgb_image'),
             DeclareLaunchArgument('depth_topic', default_value='/depth_image'),
             DeclareLaunchArgument('detection_checkpoint', default_value=''),
-            DeclareLaunchArgument('tracking_checkpoint', default_value=''),
             DeclareLaunchArgument('fx', default_value='927.17'),
             DeclareLaunchArgument('fy', default_value='927.37'),
             DeclareLaunchArgument('cx', default_value='651.32'),
             DeclareLaunchArgument('cy', default_value='349.62'),
             DeclareLaunchArgument('depth_scale', default_value='1000.0'),
             detection_node,
-            tracking_node,
         ]
     )
